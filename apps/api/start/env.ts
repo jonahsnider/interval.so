a5d91e48f5f394624e4ef72f7c5f830f4adb2c7c
Jonah Snider <jonah@jonahsnider.com>
2024-05-31 16:47:30 -0700

    fix(api): make more env vars optional and fix path to .env

--- apps/api/start/env.ts
+++ apps/api/start/env.ts
@@ -12,15 +12,15 @@
 import { Env } from '@adonisjs/core/env';
 
 // biome-ignore lint/style/noDefaultExport: This must be a default export
-export default await Env.create(new URL('../', import.meta.url), {
+export default await Env.create(new URL('../../../', import.meta.url), {
 	// biome-ignore lint/style/useNamingConvention: These are environment variables
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 	// biome-ignore lint/style/useNamingConvention: These are environment variables
-	PORT: Env.schema.number(),
+	PORT: Env.schema.number.optional(),
 	// biome-ignore lint/style/useNamingConvention: These are environment variables
 	APP_KEY: Env.schema.string(),
 	// biome-ignore lint/style/useNamingConvention: These are environment variables
-	HOST: Env.schema.string({ format: 'host' }),
+	HOST: Env.schema.string.optional({ format: 'host' }),
 	// biome-ignore lint/style/useNamingConvention: These are environment variables
-	LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+	LOG_LEVEL: Env.schema.enum.optional(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 });
