92da5b2248c01a20ddf1e751a29ae69ea722a2e0
Jonah Snider <jonah@jonahsnider.com>
2024-06-17 14:08:46 -0700

    refactor: remove center column from navbar

--- apps/web/src/components/navbar/navbar.tsx
+++ apps/web/src/components/navbar/navbar.tsx
@@ -5,28 +5,26 @@ import { ProfileMenu } from './profile-menu/profile-menu';
 
 type Props = {
 	left?: React.ReactNode;
-	center?: React.ReactNode;
 	right?: React.ReactNode;
 	className?: string;
 };
 
-export function Navbar({ children, center, left, right, className }: PropsWithChildren<Props>) {
-	// TODO: Nothing uses the center, but it still takes up space on small screens. Refactor to make this just a flex between with left/right, no center
+export function Navbar({ children, left, right, className }: PropsWithChildren<Props>) {
 	return (
 		<header className={cn('w-full py-4 bg-background border-b flex flex-col', className)}>
-			<div className='grid grid-rows-1 grid-cols-3 items-center justify-between container max-w-6xl mx-auto'>
-				<div className='flex justify-start items-center col-span-1'>
-					<Link href='/' className='text-xl font-semibold leading-none'>
-						hours.frc.sh
-					</Link>
-					{left}
-				</div>
+			<div className='container max-w-6xl mx-auto'>
+				<div className='flex justify-between'>
+					<div className='flex justify-start items-center'>
+						<Link href='/' className='text-xl font-semibold leading-none'>
+							hours.frc.sh
+						</Link>
+						{left}
+					</div>
 
-				{center && <div className='flex justify-center col-span-1'>{center}</div>}
-
-				<div className='flex justify-end col-span-1 col-start-3'>
-					<ProfileMenu />
-					{right}
+					<div className='flex justify-end'>
+						<ProfileMenu />
+						{right}
+					</div>
 				</div>
 
 				{children}
