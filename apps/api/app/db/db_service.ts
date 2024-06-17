b83bc233758c7fa82b06efd732ce7c787967d96f
Jonah Snider <jonah@jonahsnider.com>
2024-06-17 16:59:23 -0700

    feat(api): include pending meetings in combined hours stat

--- apps/api/app/db/db_service.ts
+++ apps/api/app/db/db_service.ts
@@ -7,7 +7,6 @@ import * as schema from '../../database/schema.js';
 const connection = postgres(postgresUrl.release());
 export const db = drizzle(connection, {
 	schema: { ...schema, ...relations },
-	logger: { logQuery: console.log },
 });
 
 export type Db = typeof db;
--- apps/api/app/team_stats/team_stats_service.ts
+++ apps/api/app/team_stats/team_stats_service.ts
@@ -1,7 +1,9 @@
 import assert from 'node:assert/strict';
 import { TRPCError } from '@trpc/server';
 import { convert } from 'convert';
+import { add } from 'date-fns';
 import { and, eq, gt, lt, sql, sum } from 'drizzle-orm';
+import { unionAll } from 'drizzle-orm/pg-core';
 import * as Schema from '#database/schema';
 import type { AppBouncer } from '#middleware/initialize_bouncer_middleware';
 import { db } from '../db/db_service.js';
@@ -16,19 +18,38 @@ export class TeamStatsService {
 	): Promise<number> {
 		assert(await bouncer.with('TeamPolicy').allows('read', team), new TRPCError({ code: 'FORBIDDEN' }));
 
-		// TODO: Include pending meetings in this stat by acting as though they had just ended now
-
 		const teamMembersForTeam = db
-			.select({ id: Schema.teamMembers.id })
+			.select({ id: Schema.teamMembers.id, pendingSignIn: Schema.teamMembers.pendingSignIn })
 			.from(Schema.teamMembers)
 			.where(eq(Schema.teamMembers.teamSlug, team.slug))
 			.as('team_members_for_team');
 
-		const [result] = await db
+		// We add a minute to the end time since asking Postgres to filter using the end time in the range means that it will always be *slightly* after the end time
+		// Since requests probably won't take more than a minute to go through, this small adjustment means we don't exclude too many results from Postgres, and don't introduce too much inaccuracy into the result
+		const timeRangeEndAdjusted = add(timeRange.end, { minutes: 1 });
+
+		// TODO: This either fully includes or fully excludes meetings depending on the time range. If the time range starts/ends when a meeting is in progress, should we include the partial meeting duration?
+
+		const pendingMeetingDurations = db
 			.select({
-				durationSeconds: sum(
-					sql`EXTRACT(epoch FROM ${Schema.finishedMemberMeetings.endedAt} - ${Schema.finishedMemberMeetings.startedAt})`,
+				durationSeconds: sql`EXTRACT(epoch FROM now() - ${teamMembersForTeam.pendingSignIn})`.as('duration_seconds'),
+			})
+			.from(teamMembersForTeam)
+			.innerJoin(Schema.finishedMemberMeetings, eq(teamMembersForTeam.id, Schema.finishedMemberMeetings.memberId))
+			.where(
+				and(
+					gt(teamMembersForTeam.pendingSignIn, timeRange.start),
+					// Need to manually stringify the date due to a Drizzle bug https://github.com/drizzle-team/drizzle-orm/issues/2009
+					lt(sql<Date>`now()`, timeRangeEndAdjusted.toISOString()),
 				),
+			);
+
+		const finishedMeetingDurations = db
+			.select({
+				durationSeconds:
+					sql`EXTRACT(epoch FROM ${Schema.finishedMemberMeetings.endedAt} - ${Schema.finishedMemberMeetings.startedAt})`.as(
+						'duration_seconds',
+					),
 			})
 			.from(Schema.finishedMemberMeetings)
 			.innerJoin(teamMembersForTeam, eq(Schema.finishedMemberMeetings.memberId, teamMembersForTeam.id))
@@ -36,9 +57,16 @@ export class TeamStatsService {
 				and(
 					gt(Schema.finishedMemberMeetings.startedAt, timeRange.start),
 					lt(Schema.finishedMemberMeetings.endedAt, timeRange.end),
-					eq(Schema.finishedMemberMeetings.memberId, teamMembersForTeam.id),
 				),
 			);
+
+		const allDurations = unionAll(pendingMeetingDurations, finishedMeetingDurations).as('all_durations');
+
+		const [result] = await db
+			.select({
+				durationSeconds: sum(allDurations.durationSeconds),
+			})
+			.from(allDurations);
 
 		if (!result?.durationSeconds) {
 			return 0;
