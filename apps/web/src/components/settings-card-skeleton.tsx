c9e6c9985ef44d21d31b240ea9d5edc71bc15518
Jonah Snider <jonah@jonahsnider.com>
2024-06-23 17:20:15 -0700

    feat(web): improve settings card skeleton

--- apps/web/src/components/settings-card-skeleton.tsx
+++ apps/web/src/components/settings-card-skeleton.tsx
@@ -1,17 +1,19 @@
-import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
-// TODO: Make this not horrifically ugly
 export function SettingsCardSkeleton() {
 	return (
 		<Card>
 			<CardHeader>
-				<CardTitle>
-					<Skeleton />
-				</CardTitle>
+				<Skeleton className='h-5 w-32' />
+
+				<Skeleton className='h-4 w-[32rem]' />
 			</CardHeader>
-			<CardFooter>
-				<Skeleton />
+			<CardContent>
+				<Skeleton className='h-9 w-96' />
+			</CardContent>
+			<CardFooter className='border-t px-6 py-4'>
+				<Skeleton className='h-9 w-32' />
 			</CardFooter>
 		</Card>
 	);
