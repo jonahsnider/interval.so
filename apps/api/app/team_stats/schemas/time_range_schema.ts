73e75691cdc722dd8e43865e8c04bfe3ee89f72d
Jonah Snider <jonah@jonahsnider.com>
2024-06-25 23:42:07 -0700

    fix(api): assert that end time is after start time in time range schema

--- apps/api/app/team_stats/schemas/time_range_schema.ts
+++ apps/api/app/team_stats/schemas/time_range_schema.ts
@@ -1,8 +1,15 @@
 import { z } from 'zod';
 
 // TODO: Support leaving the end time undefined, which in database logic should mean not filtering by the end time at all
-export const TimeRangeSchema = z.object({
-	start: z.date(),
-	end: z.date(),
-});
+export const TimeRangeSchema = z
+	.object({
+		start: z.date(),
+		end: z.date(),
+	})
+	.refine(
+		({ start, end }) => {
+			return start.getTime() <= end.getTime();
+		},
+		{ message: 'Start time must be before end time' },
+	);
 export type TimeRangeSchema = z.infer<typeof TimeRangeSchema>;
