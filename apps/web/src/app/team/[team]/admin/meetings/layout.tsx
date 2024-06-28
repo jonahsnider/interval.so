ac5fe02d9ad12f8d47c8f5a8e673a2be6cd66fa3
Jonah Snider <jonah@jonahsnider.com>
2024-06-28 02:12:39 -0700

    feat(web): add placeholder button for create meeting dialog

--- apps/web/src/app/team/[team]/admin/meetings/layout.tsx
+++ apps/web/src/app/team/[team]/admin/meetings/layout.tsx
@@ -1,12 +1,23 @@
+import { CreateMeetingDialog } from '@/src/components/admin/meetings/create-meeting-dialog';
 import { MainContent } from '@/src/components/main-content';
 import { PageHeader } from '@/src/components/page-header';
 import type { PropsWithChildren } from 'react';
 
+type Props = PropsWithChildren<{
+	params: {
+		team: string;
+	};
+}>;
+
 // biome-ignore lint/style/noDefaultExport: This must be a default export
-export default function AdminMeetingsPageLayout({ children }: PropsWithChildren) {
+export default function AdminMeetingsPageLayout({ children, params }: Props) {
+	const team = { slug: params.team };
+
 	return (
 		<>
-			<PageHeader title='Meetings' />
+			<PageHeader title='Meetings'>
+				<CreateMeetingDialog team={team} />
+			</PageHeader>
 			<MainContent>{children}</MainContent>
 		</>
 	);
