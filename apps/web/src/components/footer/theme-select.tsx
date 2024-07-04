e1be90fd2c8d402134b18bfd89bb4ae1b26f64e1
Jonah Snider <jonah@jonahsnider.com>
2024-07-04 03:54:49 -0700

    feat(web): animate theme select icon change

--- apps/web/src/components/footer/theme-select.tsx
+++ apps/web/src/components/footer/theme-select.tsx
@@ -10,19 +10,65 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Skeleton } from '@/components/ui/skeleton';
 import { ComputerDesktopIcon, MoonIcon, SunIcon } from '@heroicons/react/16/solid';
+import { AnimatePresence, type Variants, motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
+const MotionSunIcon = motion(SunIcon);
+const MotionMoonIcon = motion(MoonIcon);
+const MotionComputerDesktopIcon = motion(ComputerDesktopIcon);
+
 function ThemeIcon({ theme }: { theme: string | undefined }) {
-	switch (theme) {
-		case 'light':
-			return <SunIcon className='h-4 w-4' />;
-		case 'dark':
-			return <MoonIcon className='h-4 w-4' />;
-		default:
-			return <ComputerDesktopIcon className='h-4 w-4' />;
-	}
+	return (
+		<div className='relative h-4 w-4'>
+			<AnimatePresence initial={false} mode='popLayout'>
+				{theme === 'light' && (
+					<MotionSunIcon
+						key='light'
+						className='h-4 w-4 absolute'
+						variants={motionVariants}
+						initial='hidden'
+						animate='visible'
+						exit='hidden'
+					/>
+				)}
+				{theme === 'dark' && (
+					<MotionMoonIcon
+						key='dark'
+						className='h-4 w-4 absolute'
+						variants={motionVariants}
+						initial='hidden'
+						animate='visible'
+						exit='hidden'
+					/>
+				)}
+				{theme !== 'light' && theme !== 'dark' && (
+					<MotionComputerDesktopIcon
+						key='system'
+						className='h-4 w-4 absolute'
+						variants={motionVariants}
+						initial='hidden'
+						animate='visible'
+						exit='hidden'
+					/>
+				)}
+			</AnimatePresence>
+		</div>
+	);
 }
+
+const motionVariants: Variants = {
+	hidden: {
+		translateY: ['0', '-100%'],
+		opacity: 0,
+		transition: { duration: 0.15 },
+	},
+	visible: {
+		translateY: ['100%', '0%'],
+		opacity: 1,
+		transition: { duration: 0.15 },
+	},
+};
 
 export function ThemeSelect() {
 	const { setTheme, theme } = useTheme();
