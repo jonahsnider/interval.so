jonahsnider/interval.so

commit fc930ea47767198846ed027e52768cc3ecb8654a

refactor(web): remove unnecessary styles on attendance table

apps/web/src/components/team-dashboard/attendance-table/attendance-table.tsx
@@ -127,7 +127,6 @@ function InnerTable({
 }
 
 function InnerTableRow({ visible, className, member }: { visible: boolean; className?: string; member: SimpleMember }) {
-	const [animating, setAnimating] = useState(false);
 	const [checked, setChecked] = useState(member.atMeeting);
 
 	const router = useRouter();
@@ -162,14 +161,7 @@ function InnerTableRow({ visible, className, member }: { visible: boolean; class
 			initial='hidden'
 			variants={motionVariants}
 			animate={visible ? 'visible' : 'hidden'}
-			onAnimationStart={() => setAnimating(true)}
-			onAnimationComplete={() => setAnimating(false)}
-			className={clsx(
-				{
-					invisible: !(visible || animating),
-				},
-				className,
-			)}
+			className={className}
 		>
 			<TableCell className='font-medium pl-8'>{member.name}</TableCell>
 			<TableCell className='pr-8 text-right'>

commit d7ab3f8ac241ed2dce1a5cb5e81133e5c765d9ef

fix(web): undo accidental regression of attendance table hidden rows being "visible" Bug introduced in fc930ea47767198846ed027e52768cc3ecb8654a

apps/web/src/components/team-dashboard/attendance-table/attendance-table.tsx
@@ -136,6 +136,7 @@ function InnerTable({ filteredMembers, members }: { filteredMembers: SimpleMembe
 }
 
 function InnerTableRow({ visible, className, member }: { visible: boolean; className?: string; member: SimpleMember }) {
+	const [animating, setAnimating] = useState(false);
 	const [checked, setChecked] = useState(member.atMeeting);
 
 	// Trying to do this the more correct way with useOptimistic was complicated and didn't work
@@ -165,7 +166,16 @@ function InnerTableRow({ visible, className, member }: { visible: boolean; class
 			initial='hidden'
 			variants={motionVariants}
 			animate={visible ? 'visible' : 'hidden'}
-			className={className}
+			onAnimationStart={() => setAnimating(true)}
+			onAnimationComplete={() => setAnimating(false)}
+			className={clsx(
+				{
+					// This helps prevent hidden rows from being "on top" of shown rows when filtering
+					// Otherwise, you get weird behavior like hover states being applied to hidden rows
+					invisible: !(visible || animating),
+				},
+				className,
+			)}
 		>
 			<TableCell className='font-medium pl-8 whitespace-pre-wrap'>{member.name}</TableCell>
 			<TableCell className='pr-8 text-right'>