cca97755aea9fbe941d645fd4737b94b1b07a530
Jonah Snider <jonah@jonahsnider.com>
2024-07-07 21:06:54 -0700

    feat(web): hide table selection status when no rows selected

--- apps/web/src/components/data-tables/table-selection-status.tsx
+++ apps/web/src/components/data-tables/table-selection-status.tsx
@@ -1,4 +1,5 @@
 import type { Table } from '@tanstack/react-table';
+import clsx from 'clsx';
 
 // biome-ignore lint/style/useNamingConvention: This is in pascal case
 type Props<TData> = {
@@ -8,8 +9,13 @@ type Props<TData> = {
 // biome-ignore lint/style/useNamingConvention: This is in pascal case
 export function TableSelectionStatus<TData>({ table }: Props<TData>) {
 	return (
-		<div className='flex-1 text-sm text-muted-foreground'>
-			{table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
+		<div
+			className={clsx('flex-1 text-sm text-muted-foreground', {
+				'invisible': table.getFilteredSelectedRowModel().rows.length === 0,
+			})}
+		>
+			{table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row
+			{table.getFilteredRowModel().rows.length === 1 ? '' : 's'} selected.
 		</div>
 	);
 }
