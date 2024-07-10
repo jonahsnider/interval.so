27bdcdcd65c69e0f0f735d6e049b78fd1c52048d
Jonah Snider <jonah@jonahsnider.com>
2024-07-10 02:43:53 -0700

    feat(web): make <PageHeader /> support any ReactNode as title and description

--- apps/web/src/components/page-header.tsx
+++ apps/web/src/components/page-header.tsx
@@ -1,19 +1,38 @@
+import { Skeleton } from '@/components/ui/skeleton';
 import { cn } from '@/lib/utils';
-import type { PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import { type PropsWithChildren, type ReactNode, Suspense } from 'react';
 
 type Props = PropsWithChildren<{
-	title: string;
-	description?: string;
+	title: ReactNode;
+	description?: ReactNode;
 	className?: string;
 }>;
+
+export function PageHeaderTitle({
+	children,
+	className,
+}: PropsWithChildren<{
+	className?: string;
+}>) {
+	return (
+		<Suspense fallback={<Skeleton className='h-7 sm:h-9 w-56' />}>
+			<h1 className={clsx('text-3xl sm:text-4xl leading-none sm:leading-none', className)}>{children}</h1>
+		</Suspense>
+	);
+}
+
+export function PageHeaderDescription({ children }: PropsWithChildren) {
+	return <p className='text-muted-foreground'>{children}</p>;
+}
 
 export function PageHeader({ title, description, children, className }: Props) {
 	return (
 		<header className='py-8 border border-t-0 bg-background'>
 			<div className={cn('flex flex-col xs:flex-row xs:justify-between gap-4 mx-auto container max-w-6xl', className)}>
 				<div className='flex flex-col gap-4'>
-					<h1 className='text-4xl'>{title}</h1>
-					{description && <p className='text-muted-foreground'>{description}</p>}
+					{typeof title === 'string' ? <PageHeaderTitle>{title}</PageHeaderTitle> : title}
+					{typeof description === 'string' ? <PageHeaderDescription>{description}</PageHeaderDescription> : description}
 				</div>
 
 				{children}
