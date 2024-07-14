2e46e06d545750e5f0fb9ef47e43ba39dae73000
Jonah Snider <jonah@jonahsnider.com>
2024-07-14 02:26:28 -0700

    fix(web): fix modal overlay not fully exiting after batch deleting all members

--- apps/web/src/components/manager/members/members-table/batch-actions/batch-actions-dropdown.tsx
+++ apps/web/src/components/manager/members/members-table/batch-actions/batch-actions-dropdown.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AlertDialog } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import {
 	DropdownMenu,
@@ -12,7 +13,7 @@ import type { TeamMemberSchema } from '@hours.frc.sh/api/app/team_member/schemas
 import type { Table } from '@tanstack/react-table';
 import { useState } from 'react';
 import { BatchArchiveItem } from './batch-archive-item';
-import { BatchDeleteItem } from './batch-delete-item';
+import { BatchDeleteDialogContent, BatchDeleteItem } from './batch-delete-item';
 import { BatchUpdateAttendanceItem } from './batch-update-attendance-item';
 
 type Props = {
@@ -20,32 +21,29 @@ type Props = {
 };
 
 export function BatchActionsDropdown({ table }: Props) {
-	const [isDeleteMembersAlertOpen, setDeleteMembersAlertOpen] = useState(false);
-	const [dropdownOpen, setDropdownOpen] = useState(false);
+	const [dialogOpen, setDialogOpen] = useState(false);
 
 	const anySelection = table.getIsAllRowsSelected() || table.getIsSomeRowsSelected();
 
-	// TODO: If you delete all members, something about the dropdown close logic breaks and never allows pointer events again
-	// Need to stop having the dialog be a child of the dropdown
 	return (
-		<DropdownMenu open={isDeleteMembersAlertOpen ? true : dropdownOpen} onOpenChange={setDropdownOpen}>
-			<DropdownMenuTrigger asChild={true}>
-				<Button size='icon' variant='ghost' disabled={!anySelection} className='h-8 w-8 p-0 transition-opacity'>
-					<span className='sr-only'>Open menu</span>
-					<EllipsisVerticalIcon className='h-4 w-4' />
-				</Button>
-			</DropdownMenuTrigger>
+		<AlertDialog open={dialogOpen} onOpenChange={setDialogOpen}>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild={true}>
+					<Button size='icon' variant='ghost' disabled={!anySelection} className='h-8 w-8 p-0 transition-opacity'>
+						<span className='sr-only'>Open menu</span>
+						<EllipsisVerticalIcon className='h-4 w-4' />
+					</Button>
+				</DropdownMenuTrigger>
 
-			<DropdownMenuContent align='end'>
-				<BatchUpdateAttendanceItem table={table} />
-				<BatchArchiveItem table={table} />
-				<DropdownMenuSeparator />
-				<BatchDeleteItem
-					setDialogOpen={setDeleteMembersAlertOpen}
-					table={table}
-					closeDropdown={() => setDropdownOpen(false)}
-				/>
-			</DropdownMenuContent>
-		</DropdownMenu>
+				<DropdownMenuContent align='end'>
+					<BatchUpdateAttendanceItem table={table} />
+					<BatchArchiveItem table={table} />
+					<DropdownMenuSeparator />
+					<BatchDeleteItem />
+				</DropdownMenuContent>
+			</DropdownMenu>
+
+			<BatchDeleteDialogContent table={table} setDialogOpen={setDialogOpen} />
+		</AlertDialog>
 	);
 }
--- apps/web/src/components/manager/members/members-table/batch-actions/batch-delete-item.tsx
+++ apps/web/src/components/manager/members/members-table/batch-actions/batch-delete-item.tsx
@@ -13,18 +13,27 @@ import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
 import { trpc } from '@/src/trpc/trpc-client';
 import { TrashIcon } from '@heroicons/react/16/solid';
 import type { TeamMemberSchema } from '@hours.frc.sh/api/app/team_member/schemas/team_member_schema';
-import { AlertDialog } from '@radix-ui/react-alert-dialog';
 import type { Table } from '@tanstack/react-table';
 import { useState } from 'react';
 import { toast } from 'sonner';
 
+export function BatchDeleteItem() {
+	return (
+		<AlertDialogTrigger asChild={true}>
+			<DropdownMenuItem className='text-destructive focus:text-destructive focus:bg-destructive/10'>
+				<TrashIcon className='h-4 w-4 mr-2' />
+				Delete
+			</DropdownMenuItem>
+		</AlertDialogTrigger>
+	);
+}
+
 type Props = {
 	setDialogOpen: (open: boolean) => void;
 	table: Table<TeamMemberSchema>;
-	closeDropdown: () => void;
 };
 
-export function BatchDeleteItem({ setDialogOpen, table, closeDropdown }: Props) {
+export function BatchDeleteDialogContent({ setDialogOpen, table }: Props) {
 	const [toastId, setToastId] = useState<string | number | undefined>();
 
 	const selectedRows = table.getSelectedRowModel().rows;
@@ -36,7 +45,6 @@ export function BatchDeleteItem({ setDialogOpen, table, closeDropdown }: Props) 
 		onSuccess: () => {
 			toast.success(`Deleted ${selectedRows.length} members`, { id: toastId });
 			setDialogOpen(false);
-			closeDropdown();
 		},
 		onError: (error) => {
 			toast.error('An error occurred while deleting the members', {
@@ -47,33 +55,24 @@ export function BatchDeleteItem({ setDialogOpen, table, closeDropdown }: Props) 
 	});
 
 	return (
-		<AlertDialog onOpenChange={setDialogOpen}>
-			<AlertDialogTrigger asChild={true}>
-				<DropdownMenuItem className='text-destructive focus:text-destructive focus:bg-destructive/10'>
-					<TrashIcon className='h-4 w-4 mr-2' />
-					Delete
-				</DropdownMenuItem>
-			</AlertDialogTrigger>
-
-			<AlertDialogContent>
-				<AlertDialogHeader>
-					<AlertDialogTitle>Delete members</AlertDialogTitle>
-					<AlertDialogDescription>
-						Permanently delete these members and all of their recorded meetings. This action is not reversible, so
-						please continue with caution.
-					</AlertDialogDescription>
-				</AlertDialogHeader>
-				<AlertDialogFooter>
-					<AlertDialogCancel>Cancel</AlertDialogCancel>
-					<AlertDialogAction
-						className={buttonVariants({ variant: 'destructive' })}
-						onClick={() => deleteMembers.mutate({ members: selectedRows.map((row) => ({ id: row.original.id })) })}
-						disabled={deleteMembers.isPending}
-					>
-						Delete members
-					</AlertDialogAction>
-				</AlertDialogFooter>
-			</AlertDialogContent>
-		</AlertDialog>
+		<AlertDialogContent>
+			<AlertDialogHeader>
+				<AlertDialogTitle>Delete members</AlertDialogTitle>
+				<AlertDialogDescription>
+					Permanently delete these members and all of their recorded meetings. This action is not reversible, so please
+					continue with caution.
+				</AlertDialogDescription>
+			</AlertDialogHeader>
+			<AlertDialogFooter>
+				<AlertDialogCancel>Cancel</AlertDialogCancel>
+				<AlertDialogAction
+					className={buttonVariants({ variant: 'destructive' })}
+					onClick={() => deleteMembers.mutate({ members: selectedRows.map((row) => ({ id: row.original.id })) })}
+					disabled={deleteMembers.isPending}
+				>
+					Delete members
+				</AlertDialogAction>
+			</AlertDialogFooter>
+		</AlertDialogContent>
 	);
 }
