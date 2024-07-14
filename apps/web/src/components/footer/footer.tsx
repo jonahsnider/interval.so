378298f7a7a53935d2bbc54c1155cc57a01c93f6
Jonah Snider <jonah@jonahsnider.com>
2024-07-14 02:36:37 -0700

    fix(web): improve footer responsiveness on small screens

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
 	return (
 		<footer className='border-t w-full text-muted-foreground bg-background text-sm text-balance'>
 			<div className='flex justify-between mx-auto container py-4'>
-				<div className='flex flex-col sm:flex-row gap-4 sm:gap-12 lg:gap-16'>
+				<div className='flex flex-col xs:flex-row gap-4 xs:gap-8 sm:gap-12 lg:gap-16'>
 					<p>
 						Created by{' '}
 						<a className='underline' href='https://jonahsnider.com'>
