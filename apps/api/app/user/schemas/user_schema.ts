ef9decc9c37550fbeed2fa587a57cbc0531c9923
Jonah Snider <jonah@jonahsnider.com>
2024-07-15 02:23:59 -0700

    feat(web): improve input validation errors for signup flow

--- apps/api/app/user/schemas/user_schema.ts
+++ apps/api/app/user/schemas/user_schema.ts
@@ -2,6 +2,10 @@ import { z } from 'zod';
 
 export const UserSchema = z.object({
 	id: z.string().uuid(),
-	displayName: z.string().trim().min(1).max(128),
+	displayName: z
+		.string()
+		.trim()
+		.min(1, { message: "Display name can't be empty" })
+		.max(128, { message: "Display name can't be longer than 128 characters" }),
 });
 export type UserSchema = z.infer<typeof UserSchema>;
