871b5423cdd0454c8a900f1a651938e476491200
Jonah Snider <jonah@jonahsnider.com>
2024-08-09 02:22:28 -0700

    feat(web): make footer text base size

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { ThemeSelect } from './theme-select';
 
 export function Footer() {
 	return (
-		<footer className='border-t w-full text-muted-foreground bg-background text-sm text-balance'>
+		<footer className='border-t w-full text-muted-foreground bg-background text-balance'>
 			<div className='flex justify-between mx-auto container py-4'>
 				<div className='flex flex-col xs:flex-row gap-4 xs:gap-8 sm:gap-12 lg:gap-16'>
 					<p>
