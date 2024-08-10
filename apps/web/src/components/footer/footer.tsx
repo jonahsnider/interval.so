d022923dbd7ace118815ddd6701c4f7286ea8cf6
Jonah Snider <jonah@jonahsnider.com>
2024-08-10 04:46:49 -0700

    fix(web): improve text & sizing of footer on small screens

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -3,9 +3,9 @@ import { ThemeSelect } from './theme-select';
 
 export function Footer() {
 	return (
-		<footer className='border-t w-full text-muted-foreground bg-background text-balance'>
+		<footer className='border-t w-full text-muted-foreground bg-background text-sm xs:text-base'>
 			<div className='flex justify-between mx-auto container py-4'>
-				<div className='flex flex-col xs:flex-row gap-4 xs:gap-8 sm:gap-12 lg:gap-16'>
+				<div className='flex flex-col xs:flex-row gap-4 xs:gap-6 sm:gap-12 lg:gap-16'>
 					<p>
 						Created by{' '}
 						<a className='underline' href='https://jonahsnider.com'>
