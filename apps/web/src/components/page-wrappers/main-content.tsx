35ca92b44188ceff79f993e6b11d7a3a4fad52dd
Jonah Snider <jonah@jonahsnider.com>
2024-08-15 17:56:15 -0700

    fix(web): fix how <MainContent /> className prop is applied

--- apps/web/src/components/page-wrappers/main-content.tsx
+++ apps/web/src/components/page-wrappers/main-content.tsx
@@ -8,8 +8,8 @@ type Props = PropsWithChildren<{
 
 export function MainContent({ children, className }: Props) {
 	return (
-		<main className={cn('py-4 [view-transition-name:main-content] flex-1 flex', dotsStyles.dots, className)}>
-			<div className='mx-auto container max-w-6xl flex flex-col flex-1'>{children}</div>
+		<main className={cn('py-4 [view-transition-name:main-content] flex-1 flex', dotsStyles.dots)}>
+			<div className={cn('mx-auto container max-w-6xl flex flex-col flex-1', className)}>{children}</div>
 		</main>
 	);
 }
