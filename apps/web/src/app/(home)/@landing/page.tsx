91bf981695b4584068e254774323e172811cc275
Jonah Snider <jonah@jonahsnider.com>
2024-08-16 19:58:28 -0700

    fix(web): avoid unauthed error by replacing Next.js conditional rendering for home page

--- apps/web/src/app/(home)/@landing/page.tsx
+++
@@ -1,2 +0,0 @@
-// biome-ignore lint/performance/noBarrelFile: This isn't a barrel file
-export { default } from '../../home/page';
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,37 +1,39 @@
+import { TeamCards } from '@/src/components/home/team-cards';
 import { Navbar } from '@/src/components/navbar/navbar';
 import { FooterWrapper } from '@/src/components/page-wrappers/footer-wrapper';
 import { MainContent } from '@/src/components/page-wrappers/main-content';
-
 import { trpcServer } from '@/src/trpc/trpc-server';
 import { captureException } from '@sentry/nextjs';
 import { unstable_noStore as noStore } from 'next/cache';
-import type { ReactNode } from 'react';
+import LandingPage from './home/page';
 
-type Props = {
-	authed: ReactNode;
-	landing: ReactNode;
-};
+function AuthedHomePage() {
+	return (
+		<FooterWrapper>
+			<Navbar />
+
+			<MainContent>
+				<div className='flex items-center justify-center w-full'>
+					<TeamCards />
+				</div>
+			</MainContent>
+		</FooterWrapper>
+	);
+}
 
 // biome-ignore lint/style/noDefaultExport: This must be a default export
-export default async function TeamSelectConditionalLayout({ authed, landing }: Props) {
+export default async function HomePage() {
 	noStore();
 
 	try {
 		const { user } = await trpcServer.user.getSelf.query();
 
 		if (user) {
-			return (
-				<FooterWrapper>
-					<Navbar />
-
-					<MainContent>{authed}</MainContent>
-				</FooterWrapper>
-			);
+			return <AuthedHomePage />;
 		}
-
-		return landing;
 	} catch (error) {
 		captureException(error);
-		return landing;
 	}
+
+	return <LandingPage />;
 }
