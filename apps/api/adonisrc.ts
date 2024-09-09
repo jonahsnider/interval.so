8dd1c55bf6015f29a22eae9f86bd279b156aaca5
Jonah Snider <jonah@jonahsnider.com>
2024-09-09 01:21:14 -0700

    style(api): fix indentation in adonisrc comments

--- apps/api/adonisrc.ts
+++ apps/api/adonisrc.ts
@@ -3,25 +3,25 @@ import { defineConfig } from '@adonisjs/core/app';
 // biome-ignore lint/style/noDefaultExport: This must be a default export
 export default defineConfig({
 	/*
-|--------------------------------------------------------------------------
-| Commands
-|--------------------------------------------------------------------------
-|
-| List of ace commands to register from packages. The application commands
-| will be scanned automatically from the "./commands" directory.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Commands
+  |--------------------------------------------------------------------------
+  |
+  | List of ace commands to register from packages. The application commands
+  | will be scanned automatically from the "./commands" directory.
+  |
+  */
 	commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/bouncer/commands')],
 
 	/*
-|--------------------------------------------------------------------------
-| Service providers
-|--------------------------------------------------------------------------
-|
-| List of service providers to import and register when booting the
-| application
-|
-*/
+  |--------------------------------------------------------------------------
+  | Service providers
+  |--------------------------------------------------------------------------
+  |
+  | List of service providers to import and register when booting the
+  | application
+  |
+  */
 	providers: [
 		() => import('@adonisjs/core/providers/app_provider'),
 		() => import('@adonisjs/core/providers/hash_provider'),
@@ -39,24 +39,24 @@ export default defineConfig({
 	],
 
 	/*
-|--------------------------------------------------------------------------
-| Preloads
-|--------------------------------------------------------------------------
-|
-| List of modules to import before starting the application.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Preloads
+  |--------------------------------------------------------------------------
+  |
+  | List of modules to import before starting the application.
+  |
+  */
 	preloads: [() => import('#start/routes'), () => import('#start/kernel')],
 
 	/*
-|--------------------------------------------------------------------------
-| Tests
-|--------------------------------------------------------------------------
-|
-| List of test suites to organize tests by their type. Feel free to remove
-| and add additional suites.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Tests
+  |--------------------------------------------------------------------------
+  |
+  | List of test suites to organize tests by their type. Feel free to remove
+  | and add additional suites.
+  |
+  */
 	tests: {
 		suites: [],
 		forceExit: false,
