7ee361bf8e9d954c413996f9b723937389b9907a
Jonah Snider <jonah@jonahsnider.com>
2025-02-05 21:00:27 -0800

    fix(api): turn off Sentry to fix Drizzle

--- apps/api/config/sentry.ts
+++ apps/api/config/sentry.ts
@@ -7,7 +7,9 @@ export default defineConfig({
 	/**
 	 * Enable or disable Sentry
 	 */
-	enabled: app.inProduction,
+  // TODO: Turning Sentry on causes critical errors with drizzle-orm :(
+  // See: https://github.com/drizzle-team/drizzle-orm/issues/4079 and https://github.com/drizzle-team/drizzle-orm/issues/3726
+	enabled: false,
 
 	/**
 	 * The environment Sentry is running in
