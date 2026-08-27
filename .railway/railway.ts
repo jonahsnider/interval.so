c5e6c0fa63988ddce4aa652d90a2dbb1b24e0636
Jonah Snider <jonah@jonahsnider.com>
2026-08-27 10:30:31 -0700

    build: simplify Railway IaC
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- .railway/railway.ts
+++ .railway/railway.ts
@@ -5,45 +5,41 @@ const GB = 1e9;
 const region = 'us-west2';
 
 export default defineRailway(() => {
-	const Redis = redis('Redis', { region });
-	Redis.deploy = {
+	const cache = redis('Redis', { region });
+	cache.deploy = {
 		limitOverride: {
-			containers: { cpu: 1, memoryBytes: 1 * GB, diskBytes: 100 * GB },
+			containers: { cpu: 1, memoryBytes: 1 * GB, diskBytes: 50 * GB },
 		},
 	};
-	const Postgres = database('Postgres', 'postgres', {
+	const db = database('Postgres', 'postgres', {
 		image: 'ghcr.io/railwayapp-templates/postgres-ssl:16',
 		output: 'DATABASE_URL',
 		defaultMountPath: '/var/lib/postgresql/data',
 		region,
 	});
-	Postgres.deploy = {
+	db.deploy = {
 		limitOverride: {
-			containers: { cpu: 1, memoryBytes: 1 * GB, diskBytes: 100 * GB },
+			containers: { cpu: 1, memoryBytes: 1 * GB, diskBytes: 50 * GB },
 		},
 	};
-	const RedisDisk = volume('Redis disk', {
+	const cacheDisk = volume('Redis disk', {
 		alerts: { usage: { '100': {}, '80': {}, '95': {} } },
 		region: region,
 		sizeMB: 50000,
 	});
-	const PostgresDisk = volume('Postgres disk', {
+	const dbDisk = volume('Postgres disk', {
 		alerts: { usage: { '100': {}, '80': {}, '95': {} } },
 		region,
 		sizeMB: 50000,
 	});
 	const intervalSo = service('interval.so', {
 		source: github('jonahsnider/interval.so', { checkSuites: true }),
-		build: {
-			builder: 'RAILPACK',
-			buildCommand: 'pnpm --filter @interval.so/api build',
-		},
+		build: 'pnpm --filter @interval.so/api build',
 		start: 'node ./apps/api/build/bin/server.js',
 		preDeploy: 'pnpm --filter @interval.so/api migrate:apply',
 		healthcheck: '/health',
 		healthcheckTimeout: 30,
 		deploy: {
-			runtime: 'V2',
 			restartPolicyType: 'ALWAYS',
 			restartPolicyMaxRetries: 3,
 			limitOverride: {
@@ -54,21 +50,20 @@ export default defineRailway(() => {
 			},
 		},
 		domains: ['api.interval.so'],
-		networking: { privateNetworkEndpoint: 'intervalso' },
 		env: {
 			APP_KEY: preserve(),
 			COOKIE_DOMAIN: 'interval.so',
 			NODE_ENV: 'production',
-			POSTGRES_URL: Postgres.env.DATABASE_PUBLIC_URL,
+			POSTGRES_URL: db.env.DATABASE_PUBLIC_URL,
 			POSTHOG_HOST: 'https://us.i.posthog.com',
 			POSTHOG_KEY: preserve(),
-			REDIS_URL: Redis.env.REDIS_PUBLIC_URL,
+			REDIS_URL: cache.env.REDIS_PUBLIC_URL,
 			SENTRY_DSN_API: preserve(),
 		},
 	});
-	const API = group('API', [Redis, intervalSo, Postgres]);
+	const API = group('API', [cache, intervalSo, db]);
 
 	return project('Interval', {
-		resources: [RedisDisk, PostgresDisk, API],
+		resources: [cacheDisk, dbDisk, API],
 	});
 });
